7f47f674dce23bfae452e65d10c5875b1d25b6f8
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-01-25 18:22:46 +0200

    Add IsGuest Middleware To Login And Signup Routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,11 +7,15 @@ import {
   authLogout,
 } from "../controllers/auth.controller.js";
 import multer from "multer";
+import { isGuest } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/signup").get(getSignup).post(multer().any(), postSignup);
-router.route("/login").get(getLogin).post(multer().any(), postLogin);
+router
+  .route("/signup")
+  .get(isGuest, getSignup)
+  .post(multer().any(), postSignup);
+router.route("/login").get(isGuest, getLogin).post(multer().any(), postLogin);
 router.route("/logout").all(authLogout);
 
 export const authRouter = router;
