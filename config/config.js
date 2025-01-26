2669c285f38ce6d4e05e95db1ac67d887f752247
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-01-26 13:29:47 +0200

    Add clientID and clientSecretKey For Github Config

--- config/config.js
+++ config/config.js
@@ -5,5 +5,9 @@ dotenv.config();
 export const config = {
   dbUrl: process.env.MONGO_URL,
   port: process.env.PORT || 4000,
-  sessionKey: process.env.SESSION_SECRET_KEY
+  sessionKey: process.env.SESSION_SECRET_KEY,
+  github: {
+    clientID: process.env.CLIENT_ID,
+    clientSecretKey: process.env.CLIENT_SECRET_KEY,
+  },
 };
