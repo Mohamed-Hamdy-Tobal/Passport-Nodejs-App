1632a9b4c20b65a5e1b27bd6da95b1e25361f919
Mohamed-Hamdy-Tobal <hamdy3tobal@gmail.com>
2025-01-26 13:32:12 +0200

    Add Github Routes And Callback Function

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,6 +8,9 @@ import {
 } from "../controllers/auth.controller.js";
 import multer from "multer";
 import { isGuest } from "../middleware/auth.middleware.js";
+import { passportGithub } from "../config/passportGithub.config.js";
+import { handleResponse } from "../util/handleResponse.js";
+import { STATUS_CODES } from "../util/StatusCodes.js";
 
 const router = express.Router();
 
@@ -15,7 +18,34 @@ router
   .route("/signup")
   .get(isGuest, getSignup)
   .post(multer().any(), postSignup);
+
 router.route("/login").get(isGuest, getLogin).post(multer().any(), postLogin);
+
+// middleware triggers the authentication process with GitHub
+router.route("/auth/github").get(
+  passportGithub.authenticate("github", {
+    scope: ["user"],
+  })
+);
+
+// will redirect to after the user has successfully authenticated with GitHub.
+router
+  .route("/auth/github/cb")
+  .get(
+    passportGithub.authenticate("github", { failureRedirect: "/login" }),
+    (req, res) => {
+      return handleResponse(
+        res,
+        {
+          success: true,
+          status: STATUS_CODES.SUCCESS,
+          renderView: "index",
+        },
+        req
+      );
+    }
+  );
+
 router.route("/logout").all(authLogout);
 
 export const authRouter = router;
